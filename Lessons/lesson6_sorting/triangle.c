3f750cdc2c69b25892024d0cd8057f4c58e000ae
Maxwell-Norris <mnorriscs@gmail.com>
2017-05-22 22:16:34 -0400

    Codility triangle solution in C

---
+++ Lessons/lesson6_sorting/triangle.c
@@ -0,0 +1,23 @@
+int compare( const void* a, const void* b) {
+    
+    int ia = *((int*)a);
+    int ib = *((int*)b);
+    
+    if( ia == ib) return 0;
+    else if ( ia < ib ) return -1;
+    else return 1;
+}
+
+int solution(int A[], int N) {
+    
+    if(N < 3) return 0;
+
+    qsort(A, N, sizeof(*A), compare);
+    
+    for(int i = 2; i < N; ++i) {
+	// Cast to long to handle MAXINTs
+        if( (long)A[i-1] + (long)A[i-2] > A[i]) return 1;
+    }
+    
+    return 0;
+}
